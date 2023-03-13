e338849598c7ff9c251e4d1770fe8b0426b729c5
oskar <oskar.asbrink@gmail.com>
2023-03-13 13:43:24 +0100

    app setup

---
+++ client/js/Client.js
@@ -0,0 +1,16 @@
+
+class Client {
+
+    constructor( devMode, env ) {
+
+        this.type = 'Client';
+        this.devMode = devMode;
+        this.env = env;
+
+    }
+
+
+
+}
+
+export { Client };
