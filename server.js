095f679f87fc28576b5619397e379870fde8b0cb
oskar <oskar.asbrink@gmail.com>
2023-03-13 12:51:32 +0100

    Setup Three raw engine

---
+++ server.js
@@ -0,0 +1,60 @@
+// node servez C:\projects\rpg_combat\src
+
+const http = require('node:http');
+const fs = require('node:fs');
+const path = require('node:path');
+const port = 8080;
+
+http.createServer(function (request, response) {
+    console.log('request starting...');
+
+    var filePath = '.' + request.url;
+    if (filePath == './')
+        filePath = './index.html';
+
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case '.wav':
+            contentType = 'audio/wav';
+            break;
+    }
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT'){
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                response.end();
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+            console.log('response - Content-Type:'+contentType);
+        }
+    });
+
+}).listen(port);
+console.log('Server running at http://127.0.0.1:/'+port);
