1b7f92ad797a20982fbe59372cb002c2d8808820
oskar <oskar.asbrink@gmail.com>
2023-04-11 16:04:01 +0200

    fix aggro range logic

--- client/js/game/combat/ThreatDetector.js
+++ client/js/game/combat/ThreatDetector.js
@@ -12,18 +12,26 @@ class ThreatDetector {
         }
     }
 
-    registerNewKnownHostile = function(hostileChar) {
+    appropriatelyTreatKnownHostile = function(hostileChar) {
         if (this.gamePiece === GameAPI.getActivePlayerCharacter().gamePiece) {
             this.threatEvent.piece = hostileChar;
             this.threatEvent.value = true;
             evt.dispatch(ENUMS.Event.MAIN_CHAR_REGISTER_HOSTILE,  this.threatEvent)
         }
 
-        console.log("NEW HOSTILE", hostileChar)
+    //    console.log("NEW HOSTILE", hostileChar)
 
         if (this.gamePiece.getStatusByKey('faction') === 'GOOD') {
             if (hostileChar.gamePiece.getStatusByKey('faction') === 'EVIL') {
-                hostileChar.gamePiece.setStatusValue('selectedTarget', this.gamePiece);
+                let aggroRange = hostileChar.gamePiece.getStatusByKey('aggro_range')
+                let distance = MATH.distanceBetween(hostileChar.gamePiece.getPos(), this.gamePiece.getPos())
+                if (distance < aggroRange) {
+                    hostileChar.gamePiece.setStatusValue('selectedTarget', this.gamePiece);
+                    if (!this.gamePiece.getStatusByKey('selectedTarget')) {
+                        this.gamePiece.setStatusValue('selectedTarget', hostileChar.gamePiece);
+                    }
+                }
+
             }
         }
 
@@ -46,8 +54,8 @@ class ThreatDetector {
             let hostileChar = this.hostilesInRange[i];
             if (this.knownHostiles.indexOf(hostileChar) === -1) {
                 this.knownHostiles.push(hostileChar);
-                _this.registerNewKnownHostile(hostileChar);
             }
+            _this.appropriatelyTreatKnownHostile(hostileChar);
         }
 
         for (let i = 0; i < this.knownHostiles.length; i++) {
@@ -62,7 +70,7 @@ class ThreatDetector {
         let activeChars = GameAPI.getActiveScenarioCharacters();
         if (!activeChars) return;
         MATH.emptyArray(this.hostilesInRange);
-        SpatialUtils.getCharactersInRange(this.hostilesInRange, this.gamePiece, activeChars, this.gamePiece.getStatusByKey('aggro_range'));
+        SpatialUtils.getCharactersInRange(this.hostilesInRange, this.gamePiece, activeChars, 49);
         this.updateNearbyHostiles()
     }
 
